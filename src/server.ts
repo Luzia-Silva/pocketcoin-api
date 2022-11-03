47646a9bf42bbedba9e763225670a3904b3e1822
Luzia Gabriela Abreu da Silva Santos <73603007+Luzia-Silva@users.noreply.github.com>
2022-11-03 00:29:10 -0300

    install cors

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,14 @@ import swaggerDocument from "./swagger.json";
 import express from "express";
 import mongoose from "mongoose";
 import routes from "./routes";
+import cors from 'cors'
 
 const app = express();
 mongoose.connect("mongodb://localhost/pocketcoin");
-
+app.use(cors());
 app.use(express.json());
 app.use(routes);
+
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.listen(8080, () => {
